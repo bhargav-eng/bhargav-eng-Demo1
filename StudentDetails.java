b13ee81c667667cd5fd3857f2e96fd2a59650c06
bhargav-eng <245122748064@mvsrec.edu.in>
2025-07-23 12:27:25 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,11 @@
+vi StudentDetails.java
+public class StudentDetails {
+public static void main(String[] args) {
+String name = "Name";
+String rollNo = "24512274800";
+String department = "Computer Science";
+System.out.println("Student Name: " + name);
+System.out.println("Roll No: " + rollNo);
+System.out.println("Department: " + department);
+}
+}
